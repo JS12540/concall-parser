8df8d33b0d676c011a8dcbbe9d5cb5cbc0ee4f51
pranshu-raj-211 <pranshuraj65536@gmail.com>
2025-03-19 19:32:48 +0530

    add separate error logging

--- backend/log_config.py
+++ backend/log_config.py
@@ -4,7 +4,8 @@ logger = logging.Logger("concall_logger")
 logger.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler()
-file_handler = logging.FileHandler(filename="logs/app.log", mode='w')
+file_handler = logging.FileHandler(filename="logs/app.log", mode="a")
+error_file_handler = logging.FileHandler(filename="logs/error.log", mode="a")
 
 formatter = logging.Formatter(
     fmt="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
@@ -14,6 +15,9 @@ stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
+error_file_handler.setFormatter(formatter)
+error_file_handler.setLevel(logging.ERROR)
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
+logger.addHandler(error_file_handler)
