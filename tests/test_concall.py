a749b5aabecad63d620a377b4b92b1a76db17647
pranshu-raj-211 <pranshuraj65536@gmail.com>
2025-04-11 20:42:04 +0530

    tests passed for file enhancements
    tested links, pdf path, update logging, testing for no moderator next

--- tests/test_concall.py
+++ tests/test_concall.py
@@ -2,17 +2,16 @@ from concall_parser.log_config import logger
 from concall_parser.parser import ConcallParser
 
 
-def process_single_file(filepath: str):
+def process_single_file(path: str):
     """Run a single file and save its output and log."""
-    logger.debug("Starting testing for %s", filepath)
+    logger.debug("Starting testing for %s", path)
     parser = ConcallParser()
 
-    transcript_dict = parser.get_document_transcript(filepath=filepath)
+    transcript_dict = parser._get_document_transcript(path=path)
 
-    extarct_all = parser.extract_all(transcript=transcript_dict)
-
-    print(f"Extracted all: {extarct_all}")
+    extracted = parser.extract_all(transcript=transcript_dict)
+    print(f"Extracted all: {extracted}")
 
 
 if __name__ == "__main__":
-    process_single_file(r"test_documents\skf_india.pdf")
+    process_single_file(r"https://www.bseindia.com/xml-data/corpfiling/AttachHis/458af4e6-8be5-4ce2-b4f1-119e53cd4c5a.pdf")
