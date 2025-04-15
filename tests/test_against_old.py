ac0a638f43a416378ab564493d64c7f21c190593
pranshu-raj-211 <pranshuraj65536@gmail.com>
2025-04-15 13:58:06 +0530

    add regression tests
    moved test documents dir, add reg tests, reg test files, updated requirements to include pytest

---
+++ tests/test_against_old.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+
+from concall_parser.log_config import logger
+from concall_parser.parser import ConcallParser
+
+PDF_DIR = "tests/test_documents"
+
+
+@pytest.mark.parametrize("pdf_file", [
+    os.path.join(PDF_DIR, f) for f in os.listdir(PDF_DIR) if f.endswith(".pdf")
+])
+def test_pdf_parser_regression(pdf_file, data_regression):
+    """Test against saved working version of output."""
+    logger.info(f"Testing for file {pdf_file}")
+    parser = ConcallParser(path=pdf_file)
+    try:
+        result = parser.extract_all()
+        data_regression.check(result)
+    except Exception as e:
+        logger.error(f"Failed on file: {pdf_file} with error: {str(e)}")
+        raise
